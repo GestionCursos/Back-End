761bb53f256eef6a3f0f8eb574d3d1e46d3e7d8e
Alexander <juanperzezss@gmail.com>
2025-05-14 00:08:51 -0500

    se implementa el guard de forma global para toda la app

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { FirebaseGuard } from './guard/firebase.guard';
 
 async function bootstrap() {
+
   const app = await NestFactory.create(AppModule);
+  const firebaseGuard = app.get(FirebaseGuard);
+  app.useGlobalGuards(firebaseGuard);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
