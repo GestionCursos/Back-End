84d5c426530c58bb888b934c8412cdadf637f575
Romero0112 <adri0986127348@gmail.com>
2025-05-14 17:46:20 -0500

    Se agregaron las validaciones de pipes en el dto

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { FirebaseGuard } from './guard/firebase.guard';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
 
   const app = await NestFactory.create(AppModule);
   const firebaseGuard = app.get(FirebaseGuard);
   app.useGlobalGuards(firebaseGuard);
+  app.useGlobalPipes(new ValidationPipe({
+    transform: true,
+    whitelist: true,
+    forbidNonWhitelisted: true,
+  }));
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
