de549dd7f93ff6568c771f84c2b9bfad9ea66037
Kevin Peñafiel <alexpenafiel27@gmail.com>
2025-05-14 18:49:26 -0500

    Se agrego la entidad contenido_home con sus atributos

--- src/contenido_home/entities/contenido_home.entity.ts
+++ src/contenido_home/entities/contenido_home.entity.ts
@@ -1,1 +1,12 @@
-export class ContenidoHome {}
+import { Column, Entity, PrimaryColumn } from "typeorm";
+@Entity("contenido_home")
+export class ContenidoHome {
+    @PrimaryColumn()
+    id_contenido: number;
+    @Column()
+    titulo: string;
+    @Column()
+    descripcion: string;
+    @Column()
+    url_foto: string;
+}
