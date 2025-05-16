454326e34e35a616e49107e92ef76a05c38b1b45
Alexander <juanperzezss@gmail.com>
2025-05-16 07:16:46 -0500

    se agregan los atributos a los dtos de evento

--- src/evento/dto/create-evento.dto.ts
+++ src/evento/dto/create-evento.dto.ts
@@ -1,1 +1,47 @@
-export class CreateEventoDto {}
+import { IsBoolean, IsDateString, IsNotEmpty, IsNumber, IsString } from "class-validator";
+
+export class CreateEventoDto {
+    @IsString()
+    @IsNotEmpty()
+    nombre: string;
+    @IsString()
+    @IsNotEmpty()
+    tipoEvento: string;
+    @IsDateString()
+    @IsNotEmpty()
+    fechaInicio: Date;
+    @IsDateString()
+    @IsNotEmpty()
+    fechaFin: Date;
+    @IsString()
+    @IsNotEmpty()
+    modalidad: string;
+    @IsString()
+    @IsNotEmpty()
+    costo: number;
+    @IsString()
+    @IsNotEmpty()
+    categoria: string;
+    @IsString()
+    @IsNotEmpty()
+    numeroHoras: string;
+    @IsNumber()
+    @IsNotEmpty()
+    notaAprovacion: number;
+    @IsBoolean()
+    @IsNotEmpty()
+    requiereAsistencia: boolean;
+    @IsString()
+    @IsNotEmpty()
+    urlFoto: string;
+    //agregar relacion de uno a muchos con secciones
+    @IsBoolean()
+    @IsNotEmpty()
+    visible: boolean;
+    @IsString()
+    @IsNotEmpty()
+    descripcion: string;
+    @IsNumber()
+    @IsNotEmpty()
+    idOrganizador: number;
+}
