6858434ab72dd91208af7486288de98be0e60e22
Alexander <juanperzezss@gmail.com>
2025-05-15 23:22:56 -0500

    se agregan los atributos que se manejara en los dto de la entidad Organizador

--- src/organizador/dto/create-organizador.dto.ts
+++ src/organizador/dto/create-organizador.dto.ts
@@ -1,1 +1,13 @@
-export class CreateOrganizadorDto {}
+import { IsNotEmpty, IsString } from "class-validator";
+
+export class CreateOrganizadorDto {
+    @IsNotEmpty()
+    @IsString()
+    nombre: string;
+    @IsNotEmpty()
+    @IsString()
+    institucion: string;
+    @IsString()
+    @IsNotEmpty()
+    correo: string;
+}
--- src/organizador/entities/organizador.entity.ts
+++ src/organizador/entities/organizador.entity.ts
@@ -3,7 +3,7 @@ import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 @Entity("Organizador")
 export class Organizador {
     @PrimaryGeneratedColumn({name:"id_organizador"})
-    id:number;
+    id?:number;
     @Column()
     nombre:string;
     @Column()
