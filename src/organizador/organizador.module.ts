cc0bc4e237243ce0336f8dbd3cfaf2c2e8a58029
Alexander <juanperzezss@gmail.com>
2025-05-15 23:48:31 -0500

    fix: importado TypeOrmModule.forFeature en OrganizadorModule para evitar error de inyección

--- src/organizador/organizador.module.ts
+++ src/organizador/organizador.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { OrganizadorService } from './organizador.service';
 import { OrganizadorController } from './organizador.controller';
 import { Organizador } from './entities/organizador.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports:[Organizador],
+  imports:[TypeOrmModule.forFeature([Organizador])],
   controllers: [OrganizadorController],
   providers: [OrganizadorService],
 })
