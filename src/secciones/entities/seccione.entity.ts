54193bb4088294303bb11037b66cc4d66dfbffa5
Kevin Peñafiel <alexpenafiel27@gmail.com>
2025-05-19 15:36:02 -0500

    Se desarrollo el modulo de secciones

--- src/secciones/entities/seccione.entity.ts
+++ src/secciones/entities/seccione.entity.ts
@@ -1,1 +1,18 @@
-export class Seccione {}
+import { Column, Entity, PrimaryColumn, PrimaryGeneratedColumn } from 'typeorm';
+
+export class Seccione {
+  @PrimaryGeneratedColumn()
+  id_seccion?: number;
+  @Column()
+  nombre: string;
+  @Column()
+  cargo: string;
+  @Column()
+  descripcion: string;
+  @Column()
+  icono_url: string;
+  @Column()
+  orden: number;
+  @Column()
+  visible: boolean;
+}
