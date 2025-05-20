8f6accde7b3ac0a4b1328af07ec2d5ea07c43fdb
Alexander <juanperzezss@gmail.com>
2025-05-19 19:31:55 -0500

    se implementa la logica de los endpoints

--- src/facultad/facultad.service.ts
+++ src/facultad/facultad.service.ts
@@ -1,17 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateFacultadDto } from './dto/create-facultad.dto';
-import { UpdateFacultadDto } from './dto/update-facultad.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Facultad } from './entities/facultad.entity';
 
 @Injectable()
 export class FacultadService {
-  findOne(id: number) {
-    throw new Error('Method not implemented.');
+  constructor(
+     @InjectRepository(Facultad)
+     private readonly facultadRepository: Repository<Facultad>
+  ) { }
+  async findOne(id: number) {
+    const facultadEncontrada=await this.facultadRepository.findOne({ where: { id } });
+    if (!facultadEncontrada) throw new NotFoundException("No se encontro la facultad buscada")
+    return facultadEncontrada;
   }
-  findAll() {
-    throw new Error('Method not implemented.');
+  async findAll() {
+    const facultades = await this.facultadRepository.find();
+    if (!facultades) throw new NotFoundException("No se encontraron facultades");
+    return facultades;
   }
-  create(createFacultadDto: CreateFacultadDto) {
-    throw new Error('Method not implemented.');
+  async create(createFacultadDto: CreateFacultadDto) {
+    const nuevaFacultad = this.facultadRepository.create(createFacultadDto);
+    await this.facultadRepository.save(nuevaFacultad);
+    return nuevaFacultad;
   }
 
 }
