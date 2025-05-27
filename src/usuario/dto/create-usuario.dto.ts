0a3189c652be894d63dd82f744c5c822a8989340
Kevin Peñafiel <alexpenafiel27@gmail.com>
2025-05-26 23:04:34 -0500

    Se eliminan atributos inecesarios del la entidad usuario

--- src/usuario/dto/create-usuario.dto.ts
+++ src/usuario/dto/create-usuario.dto.ts
@@ -1,47 +1,43 @@
-import { IsNotEmpty, IsString } from "class-validator";
+import { IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateUsuarioDto {
-    @IsString()
-    @IsNotEmpty()
-    uid_firebase: string;
+  @IsString()
+  @IsNotEmpty()
+  uid_firebase: string;
 
-    @IsString()
-    @IsNotEmpty()
-    nombres: string;
+  @IsString()
+  @IsNotEmpty()
+  nombres: string;
 
-    @IsString()
-    @IsNotEmpty()
-    apellidos: string;
+  @IsString()
+  @IsNotEmpty()
+  apellidos: string;
 
-    @IsString()
-    @IsNotEmpty()
-    correo: string;
+  @IsString()
+  @IsNotEmpty()
+  correo: string;
 
-    @IsString()
-    @IsNotEmpty()
-    password: string;
+  @IsString()
+  @IsNotEmpty()
+  telefono: string;
 
-    @IsString()
-    @IsNotEmpty()
-    telefono: string;
+  @IsString()
+  @IsNotEmpty()
+  direccion: string;
 
-    @IsString()
-    @IsNotEmpty()
-    direccion: string;
+  @IsString()
+  @IsNotEmpty()
+  rol: string;
 
-    @IsString()
-    @IsNotEmpty()
-    rol: string;
+  @IsString()
+  @IsNotEmpty()
+  carrera: string;
 
-    @IsString()
-    @IsNotEmpty()
-    carrera: string;
+  @IsString()
+  @IsNotEmpty()
+  estado: string;
 
-    @IsString()
-    @IsNotEmpty()
-    estado: string;
-    
-    @IsString()
-    @IsNotEmpty()
-    url_foto: string;
+  @IsString()
+  @IsNotEmpty()
+  url_foto: string;
 }
--- src/usuario/entities/usuario.entity.ts
+++ src/usuario/entities/usuario.entity.ts
@@ -1,39 +1,40 @@
-import { Column, DeleteDateColumn, Entity, PrimaryColumn } from "typeorm";
+import { Column, DeleteDateColumn, Entity, PrimaryColumn } from 'typeorm';
 
-@Entity("Usuarios")
+@Entity('Usuarios')
 export class Usuario {
-    @PrimaryColumn()
-    uid_firebase: string;
-  
-    @Column({name: "nombres"})
-    nombres: string;
-  
-    @Column()
-    apellidos: string;
-  
-    @Column()
-    correo: string;
-  
-    @Column()
-    password: string;
-  
-    @Column()
-    telefono: string;
-  
-    @Column()
-    direccion: string;
-  
-    @Column()
-    rol: string;
-  
-    @Column()
-    carrera: string;
-  
-    @Column()
-    estado: string;
-  
-    @Column()
-    url_foto: string;
-    @DeleteDateColumn({ name: 'fecha_eliminacion', nullable: true ,default:null})
-    fechaEliminacion?: Date;
+  @PrimaryColumn()
+  uid_firebase: string;
+
+  @Column({ name: 'nombres' })
+  nombres: string;
+
+  @Column()
+  apellidos: string;
+
+  @Column()
+  correo: string;
+
+  @Column()
+  telefono: string;
+
+  @Column()
+  direccion: string;
+
+  @Column()
+  rol: string;
+
+  @Column()
+  carrera: string;
+
+  @Column()
+  estado: string;
+
+  @Column()
+  url_foto: string;
+  @DeleteDateColumn({
+    name: 'fecha_eliminacion',
+    nullable: true,
+    default: null,
+  })
+  fechaEliminacion?: Date;
 }
