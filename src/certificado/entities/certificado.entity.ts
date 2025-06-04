48f88ae66a2a034e63f8ec6efb9b3b5d85a9a62f
Kevin Peñafiel <alexpenafiel27@gmail.com>
2025-06-04 18:15:15 -0500

    Se crea la entidad para el modulo de certficados

--- src/certificado/entities/certificado.entity.ts
+++ src/certificado/entities/certificado.entity.ts
@@ -1,1 +1,13 @@
-export class Certificado {}
+import { Inscripcion } from "src/inscripcion/entities/inscripcion.entity";
+import { Column, Entity, JoinColumn, ManyToOne, OneToOne, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity('certificados')
+export class Certificado {
+    @PrimaryGeneratedColumn({ name: 'id_certificado' })
+    idCertificado:number;
+    @OneToOne(()=>Inscripcion,(inscripcion)=>inscripcion.idInscripcion)
+    @JoinColumn({name:'id_inscripcion'})
+    idInscripcion:Inscripcion;
+    @Column({name:'url_certificado'})
+    urlCertificado:string;
+}
