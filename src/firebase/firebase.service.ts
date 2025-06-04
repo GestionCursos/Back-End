1eccebe5a6945da4a28250df73a6a0d5f69f5edf
Alexander <juanperzezss@gmail.com>
2025-06-04 17:54:08 -0500

    implementacion de metodo pra subir archivos al firebase storage

--- src/firebase/firebase.service.ts
+++ src/firebase/firebase.service.ts
@@ -8,7 +8,7 @@ export class FirebaseService {
         type: process.env.FIREBASE_TYPE,
         project_id: process.env.FIREBASE_PROJECT_ID,
         private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-        private_key: process.env.FIREBASE_PRIVATE_KEY,
+        private_key: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
         client_email: process.env.FIREBASE_CLIENT_EMAIL,
         client_id: process.env.FIREBASE_CLIENT_ID,
         auth_uri: process.env.FIREBASE_AUTH_URI,
@@ -29,12 +29,13 @@ export class FirebaseService {
             const serviceAccountData = JSON.parse(JSON.stringify(this.credential)) as admin.ServiceAccount;
             admin.initializeApp({
                 credential: admin.credential.cert(serviceAccountData),
+                storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
             });
         }
     }
 
     //funcion para validar el token de firebase
-       async verifyIdToken(idToken: string) {
+    async verifyIdToken(idToken: string) {
         try {
             const decodedToken = await admin.auth().verifyIdToken(idToken);
             return decodedToken;
@@ -43,5 +44,28 @@ export class FirebaseService {
         }
     }
 
+
+    async uploadFile(fileBuffer: Buffer, filename: string, mimeType: string): Promise<string> {
+        const bucket = admin.storage().bucket();
+        const file = bucket.file(filename);
+
+        const stream = file.createWriteStream({
+            metadata: {
+                contentType: mimeType,
+            },
+        });
+
+        return new Promise((resolve, reject) => {
+            stream.on('error', (err) => reject(err));
+
+            stream.on('finish', async () => {
+                await file.makePublic();
+                const publicUrl = `https://storage.googleapis.com/${bucket.name}/${file.name}`;
+                resolve(publicUrl);
+            });
+
+            stream.end(fileBuffer);
+        });
+    }
 
 }
