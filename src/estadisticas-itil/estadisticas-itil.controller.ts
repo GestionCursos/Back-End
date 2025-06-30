d20088b749b140ad30cada34b8b71b5e7cb7b6e1
Alexander Tasinchano <132286484+Alexande6055@users.noreply.github.com>
2025-06-30 01:25:49 -0500

    creacion de end-points para consumir los datos desde el front-end

--- src/estadisticas-itil/estadisticas-itil.controller.ts
+++ src/estadisticas-itil/estadisticas-itil.controller.ts
@@ -1,7 +1,37 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { EstadisticasItilService } from './estadisticas-itil.service';
+import { Public } from 'src/guard/decorators/public.decorator';
 
 @Controller('estadisticas-itil')
 export class EstadisticasItilController {
-  constructor(private readonly estadisticasItilService: EstadisticasItilService) {}
+  constructor(private readonly estadisticasItilService: EstadisticasItilService) { }
+
+
+  @Get()
+  @Public()
+  obtenerDesarrolladores() {
+    return this.estadisticasItilService.obtenerDesarrolladores();
+  }
+  @Get('cambios-recientes')
+  @Public()
+  getCambiosRecientes() {
+    return this.estadisticasItilService.obtenerCambiosRecientes();
+  }
+
+  @Get('estados')
+  @Public()
+  getEstados() {
+    return this.estadisticasItilService.obtenerEstadisticasEstadoCambio();
+  }
+
+  @Get('datosEvolucion')
+  @Public()
+  getEvolucion() {
+    return this.estadisticasItilService.obtenerDatosEvolucion();
+  }
+  @Get('tiposCambio')
+  @Public()
+  getTipoCambio() {
+    return this.estadisticasItilService.obtenerTiposCambios();
+  }
 }
